e5b36e8af270a098824621311fb8661f6411fbf5
Jair Copete <33769954+JairCopete17@users.noreply.github.com>
2024-04-19 22:08:56 -0500

    fix: rfid working :D

--- sys_esp32.ino
+++ sys_esp32.ino
@@ -1,38 +1,41 @@
 /////////////////////////
 /////// Entradas ////////
-#define NR_OF_READERS 2
+#define NR_OF_READERS 4
 #define RST_PIN       22
 #define SS_1_PIN      21
 #define SS_2_PIN      5
 #define SERVO_IN_PIN  4 
 #define SERVO_OUT_PIN 2
 /////////////////////////
-///// Parking Bici //////
-//#define SS_3_PIN    A
-//#define FIN_CAR_PIN A
-//#define ELECTOIMAN  A
-/////////////////////////
 //// Parking Scooter ////
-//#define SS_4_PIN    A
+#define SS_3_PIN      16
+/////////////////////////
+///// Parking Bici //////
+#define SS_4_PIN      17
+#define FIN_CAR_PIN   14
+//#define ELECTOIMAN  A
 /////////////////////////
 
 #include <SPI.h>
 #include <MFRC522.h>
-#include <NewPing.h>
 #include <ESP32Servo.h>
 
-byte ssPins[] = {SS_1_PIN, SS_2_PIN};
+byte ssPins[] = {SS_1_PIN, SS_2_PIN, SS_3_PIN, SS_4_PIN};
+
 MFRC522 mfrc522[NR_OF_READERS];
 Servo servoIn;
 Servo servoOut;
 Servo pinza;
-NewPing sonar(17, 18, 200);
 
 int cnt = 0;
+int dist = 0;
+int park_bici = 0;
 int initposIn = 140;
 int finalposIn = 35;
 int initposOut = 90;
 int finalposOut = 0;
+int initposPinza = 140;
+int finalposPinza = 35;
 byte LecturaUID[4];
 byte Usuario1[4]= {0x0C, 0x51, 0x8B, 0x17} ;    // UID de llavero
 byte Usuario2[4]= {0xB4, 0x56, 0xC9, 0xE7} ;    // UID de carnet de la universidad Santiago
@@ -55,33 +58,30 @@ void setup() {
   servoOut.attach(SERVO_OUT_PIN, 500, 2400);
   servoOut.write(initposOut);
   pinza.attach(15, 500, 2400);
-  pinza.write(0);
+  pinza.write(initposPinza);
 
-  //  pinMode(14, OUTPUT);  // Parking 1
-  //  pinMode(12, OUTPUT);
-  //  pinMode(13, OUTPUT);
-  //  pinMode(25, OUTPUT);  // Parking 2
-  //  pinMode(26, OUTPUT);
-  //  pinMode(27, OUTPUT);
-  //  pinMode(33, OUTPUT);  // Parking bici
-  //  pinMode(32, OUTPUT);  // Parking scooter
-  //  
-  //  digitalWrite(14, HIGH); delay(100);
-  //  digitalWrite(12, HIGH); delay(100);
-  //  digitalWrite(13, HIGH); delay(100);
-  //  digitalWrite(25, HIGH); delay(100);
-  //  digitalWrite(26, HIGH); delay(100);
-  //  digitalWrite(27, HIGH); delay(100);
-  //  digitalWrite(32, HIGH); delay(100);
-  //  digitalWrite(33, HIGH); delay(100);
-
+  pinMode(33, OUTPUT); // LED BICI
+  pinMode(32, OUTPUT); // LED SCOOTER
   Serial.println("Inicio del registro");
 }
 
 void loop() {
-  ultrasonido();
-  digitalWrite(14, HIGH); delay(100);
-  digitalWrite(25, HIGH); delay(100);
+  //Calculamos la distancia en cm
+  dist = 0.01723 * readUltrasonicDistance(13, 12);
+  if (dist <= 3) {                            //Mostramos la distancia
+    Serial.println("Scooter parqueada");
+    pinza.write(finalposPinza); 
+    digitalWrite(32, HIGH);
+  } else {
+    pinza.write(initposPinza);
+    digitalWrite(32, LOW);
+  }
+  
+  int park_bici = digitalRead(FIN_CAR_PIN);
+  if (!park_bici) {
+    Serial.println("Bicicleta parqueada");
+    digitalWrite(33, HIGH);
+  } else digitalWrite(33, LOW);
   
   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
     if (mfrc522[reader].PICC_IsNewCardPresent() && mfrc522[reader].PICC_ReadCardSerial()) {
@@ -100,8 +100,6 @@ void loop() {
         servoOut.write(finalposOut);
         delay(2500);
         servoOut.write(initposOut);
-
-        digitalWrite(14, HIGH);
       }
       
       if (reader == 1) {
@@ -111,6 +109,14 @@ void loop() {
         servoIn.write(initposIn);
       }
 
+      if (reader == 2) {
+        Serial.println("LECTOR DE SCOOTER DETECTADO");
+      }
+
+      if (reader == 3) {
+        Serial.println("LECTOR DE BICI DETECTADO");
+      }
+      
       mfrc522[reader].PICC_HaltA();
       mfrc522[reader].PCD_StopCrypto1();
     } //if (mfrc522[reader].PICC_IsNewC
@@ -124,13 +130,13 @@ void dump_byte_array(byte *buffer, byte bufferSize) {
   }
 }
 
-void ultrasonido(){
-  // Trigger D17 Echo D18
-  float distance_cm = sonar.ping_cm();
-  Serial.print("Distancia: ");
-  Serial.print(distance_cm);
-  Serial.println(" cm");
-
-  if (distance_cm >= 10.0) pinza.write(45);
-    else pinza.write(0);
+long readUltrasonicDistance(int triggerPin, int echoPin) {
+  pinMode(triggerPin, OUTPUT);        // Iniciamos el pin del emisor de reuido en salida
+  digitalWrite(triggerPin, LOW);      // Apagamos el emisor de sonido
+  delayMicroseconds(2);               // Retrasamos la emision de sonido por 2us
+  digitalWrite(triggerPin, HIGH);     // Comenzamos a emitir sonido
+  delayMicroseconds(10);              // Retrasamos la emision de sonido por 10uS
+  digitalWrite(triggerPin, LOW);      // Apagamos el emisor de sonido
+  pinMode(echoPin, INPUT);            // Comenzamos a escuchar el sonido
+  return pulseIn(echoPin, HIGH);      // Calculamos el tiempo que tardo en regresar el sonido
 }
